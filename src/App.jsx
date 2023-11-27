fa10f9a9155e929ed6e7eff257ae2f4b3dddb11b
Juan Rassa <jmrassa.dev@gmail.com>
2023-11-27 21:26:26 +0000

    Complete Iteration 1, 2 and 3.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import "./App.css";
+import { useState } from 'react';
+import foodsJson from './foods.json';
+import FoodBox from './components/FoodBox';
+import './App.css';
 
 function App() {
+  const [foodsData] = useState(foodsJson);
+  console.log(foodsData);
   return (
-    <div className="App">
+    <div className='App'>
       <h1>LAB | React IronNutrition</h1>
+      {foodsData.map(food => {
+        return <FoodBox key={food.id} food={food} />;
+      })}
     </div>
   );
 }
